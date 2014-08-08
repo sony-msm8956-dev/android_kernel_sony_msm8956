35f1a05e739be8a7f4950eb80a668e9d80f731b0
Russell King <rmk+kernel@arm.linux.org.uk>
2014-08-08 10:56:34 +0100

    BACKPORT: ARM: wire up getrandom syscall
    Clean cherry pick of eb6452537b280652eee66801ec97cc369e27e5d8.
    
    Add the new getrandom syscall for ARM.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    
    Bug: http://b/29621447
    Change-Id: I6d50b57f3a61fbf9102c69103b9a5b7ebf239860
    (cherry picked from commit eb6452537b280652eee66801ec97cc369e27e5d8)
    [flex1911]: backport to 3.4

--- arch/arm/include/uapi/asm/unistd.h
+++ arch/arm/include/uapi/asm/unistd.h
@@ -412,6 +412,7 @@
 #define __NR_renameat2			(__NR_SYSCALL_BASE+382)
 */
 #define __NR_seccomp			(__NR_SYSCALL_BASE+383)
+#define __NR_getrandom			(__NR_SYSCALL_BASE+384)
 
 /*
  * This may need to be greater than __NR_last_syscall+1 in order to
@@ -427,6 +428,14 @@
 #define __ARM_NR_usr26			(__ARM_NR_BASE+3)
 #define __ARM_NR_usr32			(__ARM_NR_BASE+4)
 #define __ARM_NR_set_tls		(__ARM_NR_BASE+5)
+
+/*
+ * This may need to be greater than __NR_last_syscall+1 in order to
+ * account for the padding in the syscall table
+ */
+#ifdef __KERNEL__
+#define __NR_syscalls  (388)
+#endif
 
 /*
  * *NOTE*: This is a ghost syscall private to the kernel.  Only the
