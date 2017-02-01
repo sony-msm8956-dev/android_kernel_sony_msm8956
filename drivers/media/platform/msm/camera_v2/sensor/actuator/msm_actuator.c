14078dfd44abdc1aaa2797c45678fa0b80f860b6
Angelo G. Del Regno <kholk11@gmail.com>
2017-02-01 19:16:43 +0100

    media: msm: camera_v2: Do not fail when actuator has no GPIOs
    The fact that we need a GPIO (or pinctrl handle) for the actuator(s)
    is not necessarily true... at least for us: we manage power by only
    using regulator controls and we shut it down completely when we
    don't need it.

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -2287,9 +2287,10 @@ static int32_t msm_actuator_platform_probe(struct platform_device *pdev)
 		rc = msm_camera_pinctrl_init(
 			&(msm_actuator_t->pinctrl_info), &(pdev->dev));
 		if (rc < 0) {
-			pr_err("ERR:%s: Error in reading actuator pinctrl\n",
-				__func__);
+			pr_err("ERR: Error in reading actuator pinctrl. \
+			Not fatal, going on.\n");
 			msm_actuator_t->cam_pinctrl_status = 0;
+			rc = 0;
 		}
 	}
 
